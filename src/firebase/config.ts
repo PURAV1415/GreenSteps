2253edc2b59e01dc0363f545bac96886939354e4
Purav M asher <puravaaher@gmail.com>
2026-01-28 08:48:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-506870936-c2710",
+  "appId": "1:157167925264:web:21a7e3a02c408075cbb295",
+  "apiKey": "AIzaSyAHSWF600RyhiQNy5Hv0IZdryCKhSeOLts",
+  "authDomain": "studio-506870936-c2710.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "157167925264"
+};
